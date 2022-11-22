63e4ab65bed477220b3185d86551bb3e7f7936b7
Lyon <lyon.b.lee@gmail.com>
2022-11-21 23:22:01 -0500

    headTest refactored using Mocha & Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([5,6,7]));
-console.log(head([]));
-
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("prints out the first element in the array for head([5,6,7])", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns undefined if array is empty", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
