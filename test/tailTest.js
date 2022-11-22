9ac231624810d626b8b77f99e90d5f9d05b1cfe2
Lyon <lyon.b.lee@gmail.com>
2022-11-21 23:33:19 -0500

    tailTest.js refactored to be compatible with mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+  it("returns the length of original array even after the function was used", () => {
+    assert.strictEqual(words.length, 3)
+  });
+});
